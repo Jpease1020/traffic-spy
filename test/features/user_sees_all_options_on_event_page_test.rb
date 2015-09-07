472b57a64754b5e11241f5e872c807af72cf283e
Justin Pease <justinpease2@gmail.com>
2015-09-07 15:03:42 -0600

    tests event index page and adds header partial

--- test/features/user_sees_all_options_on_event_page_test.rb
+++ test/features/user_sees_all_options_on_event_page_test.rb
@@ -21,8 +21,37 @@ class EvenPageTest < FeatureTest
                         "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
                         "resolutionWidth":"1920",
                         "resolutionHeight":"1280",
+                        "referredBy":"http://jumpstartlab.com",
+                        "requestType":"GET",
+                        "eventName": "socialLogin",
                         "requestedAt":"2013-02-16 21:38:28 -0700",
                         "respondedIn":37,"ip":"63.29.38.211"}'
+    post "/sources/jumpstartlab/data", @payload
+    end
+
+    def test_page_shows_header
+      visit '/sources/jumpstartlab/events'
+      within("#header") do
+        assert page.has_content?("Events Dashboard")
+        assert page.has_content?("Jumpstartlab")
+      end
+    end
+
+    def test_page_shows_most_received_to_least_received
+      visit '/sources/jumpstartlab/events'
+      within("#most_received_events") do
+        assert page.has_content?("Most Received to Least Received Events")
+        # save_and_open_page
+        assert page.has_content?("socialLogin")
+      end
+    end
+
+    def test_page_shows_even_specific_data
+      visit '/sources/jumpstartlab/events'
+      within("#event_specific_data") do
+        assert page.has_content?("Event Specifc Data")
+        assert page.has_content?("http://localhost:9393/sources/jumpstartlab/events/socialLogin")
+      end
     end
 
     def teardown
